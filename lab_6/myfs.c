ca31698c893d8e23713ebd7db85207359fbd0174
kateVW <k>
2025-06-07 21:07:31 +0300

    vw

--- lab_6/myfs.c
+++ lab_6/myfs.c
@@ -3,13 +3,13 @@
 # include <linux/init.h>
 # include <linux/fs.h>
 # include <linux/time.h>
-#include <linux/slab.h>      // For kmem_cache_free, kfree
-#include <linux/slab_def.h>  // For kmem_cache related functions
+# include <linux/mnt_idmapping.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("kathrine");
+MODULE_AUTHOR("Impervguin");
+MODULE_DESCRIPTION("Module with simple vfs");
 
-#define MYFS_MAGIC_NUMBER 0x13131313
+#define MYFS_MAGIC_NUMBER 0x42424242
 #define MYFS_SLAB_NAME "myfs_slab"
 #define MYFS_CACHE_SIZE 1
 
@@ -29,50 +29,58 @@ struct myfs_sb_info {
     int num_cache;
 };
 
+
 static struct kmem_cache *myfs_slab;
 
-static void myfs_put_super(struct super_block * sb) {
-    printk(KERN_INFO "+ myfs: super block destroyed\n");
-}
-
 static struct super_operations const myfs_super_ops = {
-    .put_super = myfs_put_super,
     .statfs = simple_statfs,
     .drop_inode = generic_delete_inode,
 };
 
+
 static struct inode * myfs_make_inode(struct super_block * sb, int mode)
 {
     struct inode * ret = new_inode(sb);
-    if (ret) {
-        inode_init_owner(&init_user_ns, ret, NULL, mode);
+    if (ret)
+    {
+        inode_init_owner(&nop_mnt_idmap, ret, NULL, mode);
         ret->i_size = PAGE_SIZE;
-        ret->i_atime = ret->i_mtime = ret->i_ctime = current_time(ret);
+        struct timespec64 cur = current_time(ret);
+        inode_set_atime_to_ts(ret, cur);
+        inode_set_mtime_to_ts(ret, cur);
+        inode_set_ctime_to_ts(ret, cur);
         ret->i_ino = 1;
     }
     return ret;
 }
 
-static int myfs_fill_super(struct super_block *sb, void *data, int silent)
+
+static int myfs_fill_sb(struct super_block *sb, void *data, int silent)
 {
-    struct inode *root = NULL;
+    struct inode *root_inode;
     sb->s_blocksize = PAGE_SIZE;
     sb->s_blocksize_bits = PAGE_SHIFT;
     sb->s_magic = MYFS_MAGIC_NUMBER;
     sb->s_op = &myfs_super_ops;
 
-    root = myfs_make_inode(sb, S_IFDIR | 0755);
-    if (!root) {
-        printk(KERN_ERR "+ myfs: error myfs_make_inode\n");
+
+    root_inode = myfs_make_inode(sb, S_IFDIR | 0755);
+    if (!root_inode) {
+        printk(KERN_ERR "+ myfs: myfs_make_inode error\n");
         return -ENOMEM;
     }
-    root->i_op = &simple_dir_inode_operations;
-    root->i_fop = &simple_dir_operations;
 
-    sb->s_root = d_make_root(root);
+    struct timespec64 cur = current_time(root_inode);
+    inode_set_atime_to_ts(root_inode, cur);
+    inode_set_mtime_to_ts(root_inode, cur);
+    inode_set_ctime_to_ts(root_inode, cur);
+    root_inode->i_op = &simple_dir_inode_operations;
+    root_inode->i_fop = &simple_dir_operations;
+
+    sb->s_root = d_make_root(root_inode);
     if (!sb->s_root) {
-        printk(KERN_ERR "+ myfs: error d_make_root\n");
-        iput(root);
+        printk(KERN_ERR "+ myfs: d_make_root error\n");
+        iput(root_inode);
         return -ENOMEM;
     }
 
@@ -82,8 +90,7 @@ static int myfs_fill_super(struct super_block *sb, void *data, int silent)
 
 static void kill_myfs(struct super_block *sb) {
     struct myfs_sb_info *sbi = sb->s_fs_info;
-    int i;
-    for (i = 0; i < sbi->num_cache; i++) {
+    for (int i = 0; i < sbi->num_cache; i++) {
         struct myfs_cache_entry *entry = sbi->cache[i];
         if (entry) {
             kmem_cache_free(myfs_slab, entry);
@@ -96,8 +103,7 @@ static void kill_myfs(struct super_block *sb) {
 }
 
 static struct dentry *myfs_mount(struct file_system_type *fs_type, int flags, const char *dev_name, void *data) {
-    int i;
-    struct dentry *root = mount_bdev(fs_type, flags, dev_name, data, myfs_fill_super);
+    struct dentry *root = mount_bdev(fs_type, flags, dev_name, data, myfs_fill_sb);
     if (IS_ERR(root)) {
         printk(KERN_ERR "+ myfs: mount_bdev failed\n");
         return root;
@@ -121,7 +127,7 @@ static struct dentry *myfs_mount(struct file_system_type *fs_type, int flags, co
         return ERR_PTR(-ENOMEM);
     }
 
-    for (i = 0; i < sbi->num_cache; i++) {
+    for (int i = 0; i < sbi->num_cache; i++) {
         struct myfs_cache_entry *entry = kmem_cache_alloc(myfs_slab, GFP_KERNEL);
         if (!entry) {
             printk(KERN_ERR "+ myfs: slab allocation failed for entry %d\n", i);
@@ -141,21 +147,22 @@ static struct dentry *myfs_mount(struct file_system_type *fs_type, int flags, co
 
 
 struct file_system_type myfs_type = {
-    .owner =    THIS_MODULE,
-    .name =     "myfs",
-    .mount =    myfs_mount,
-    .kill_sb =  kill_myfs,
+    .owner = THIS_MODULE,
+    .name = "myfs",
+    .mount = myfs_mount,
+    .kill_sb = kill_myfs,
     .fs_flags = FS_REQUIRES_DEV,
 };
 
 static int __init myfs_init(void) {
-    int ret;
     printk(KERN_INFO "+ myfs: init\n");
+    printk(KERN_INFO "+ myfs: sizeof(struct myfs_cache_entry) = %d\n", sizeof(struct myfs_cache_entry));
+    printk(KERN_INFO "+ myfs: sizeof(struct myfs_inode) = %d\n", sizeof(struct myfs_inode));
     
     myfs_slab = kmem_cache_create(MYFS_SLAB_NAME, 
         sizeof(struct myfs_cache_entry),
         0, 
-        0, //SLAB_HWCACHE_ALIGN | SLAB_PANIC | SLAB_ACCOUNT,
+        SLAB_HWCACHE_ALIGN | SLAB_PANIC | SLAB_ACCOUNT,
         NULL);
     
     if (!myfs_slab) {
@@ -164,7 +171,7 @@ static int __init myfs_init(void) {
     }
     printk(KERN_INFO "+ myfs: slab cache created\n");
 
-    ret = register_filesystem(&myfs_type);
+    int ret = register_filesystem(&myfs_type);
     if (ret) {
         printk(KERN_ERR "+ myfs: register_filesystem failed\n");
         return ret;
@@ -174,9 +181,8 @@ static int __init myfs_init(void) {
 }
 
 static void __exit myfs_exit(void) {
-    int ret;
     printk(KERN_INFO "+ myfs: exit\n");
-    ret = unregister_filesystem(&myfs_type);
+    int ret = unregister_filesystem(&myfs_type);
     if (ret) {
         printk(KERN_ERR "+ myfs: unregister_filesystem failed\n");
         return;
